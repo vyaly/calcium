39e6a6262269156eb4319eb9785b2a91d5bb208d
Vy <vyaly@berkeley.edu>
2014-11-12 18:34:59 -0800

    first pages

---
+++ js/food-color.js
@@ -0,0 +1,16 @@
+var food_table = {
+    "yogurt":,
+    "milk":,
+    "cheese":,
+    "ice_cream":,
+    "almond":,
+    "broccoli":,
+
+};
+
+$(document).ready(function() {
+	foods = $("#food");
+    for (var i=0; i < foods; i++) {
+
+    }
+});
